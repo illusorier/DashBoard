2a0b0772ec900f50dc27e526748de10587909897
Weiyehui <illusorier@gmail.com>
2017-06-05 14:28:05 +0800

    add styles folder

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,16 +17,22 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <modal></modal>
   </div>
 </template>
 
-<script>
+<script type="text/babel">
+import Modal from '../components/Modal/Modal.vue'
+
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    Modal
   }
 }
 </script>
--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -1,5 +1,26 @@
-<template></template>
+<template>
+  <div>
+    <div class="modal"></div>
+    <div class="modal-overlay">
+      <div class="modal-footer">
+        <button>{{ okText }}</button>
+      </div>
+    </div>
+  </div>
+</template>
 
-<script></script>
+<style lang="scss" src="./Modal.scss"></style>
 
-<style></style>
+<script type="text/babel">
+  export default {
+    created () {
+      console.log('modal loaded')
+    },
+    props: {
+      okText: {
+        type: String,
+        default: '确认'
+      }
+    }
+  }
+</script>
